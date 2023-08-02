9375c01810bdfed252a4126f1de92c4a0334a107
duleid <duleid@sfu.ca>
2023-08-01 21:30:28 -0700

    try to fix datetime64 type issue #1

--- app.py
+++ app.py
@@ -70,9 +70,11 @@ with right_column:
 st.markdown("""---""")
 
 # SALES BY PRODUCT LINE [BAR CHART]
-sales_by_product_line = (
-    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
-)
+# sales_by_product_line = (
+#     df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+# )
+sales_by_product_line = df_selection.groupby(by=["Product line"])["Total"].sum().sort_values(ascending=False) 
+
 fig_product_sales = px.bar(
     sales_by_product_line,
     x="Total",
