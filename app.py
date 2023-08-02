18f17eff508ebb64ae1699b46c7b739f64a59c78
duleid <duleid@sfu.ca>
2023-08-01 22:05:24 -0700

    fix#6

--- app.py
+++ app.py
@@ -71,8 +71,9 @@ st.markdown("""---""")
 
 # SALES BY PRODUCT LINE [BAR CHART]
 sales_by_product_line = (
-    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+    df_selection.groupby(by=["Product line"])["Total"].sum().reset_index().sort_values(by="Total")
 )
+
 
 
 fig_product_sales = px.bar(
